e9e74c5de679ff28bf0ece584c9441f21f6adf5a
Noah A Klem <noahaklem@gmail.com>
2023-09-21 12:44:37 -0600

    orm methods

--- lib/student.rb
+++ lib/student.rb
@@ -5,9 +5,49 @@ class Student
   attr_accessor :name, :grade
   attr_reader :id
 
+
   def initialize(name, grade, id=nil)
     @id = id
     @name = name
     @grade = grade
+    @db = DB[:conn].execute
   end
+
+  def self.create_table
+    sql = <<- SQL
+      CREATE TABLE students (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        grade INTEGER
+      )
+    SQL
+
+    DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    sql = <<- SQL
+      DROP TABLE students
+    SQL
+
+   db.execute(sql)
+  end
+
+  def save
+    sql = <<- SQL
+      INSERT INTO students (name, grade)
+      VALUES (?, ?)
+    SQL
+
+    db.execute(sql, self.name, self.grade)
+
+    @id = db.execute("SELECT last_insert_rowid() FROM students")[0][0]
+  end
+
+  def self.create(name:, grade:)
+    student = Student.new(name, grade)
+    student.save
+    student
+  end
+
 end
